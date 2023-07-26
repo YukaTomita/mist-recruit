127f31c729fb77dfa783a8831c2a2a73a1b7b47d
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-26 14:35:25 +0900

    syuusei

--- js/upperclassman.js
+++ js/upperclassman.js
@@ -15,37 +15,6 @@ function hidePopup(popupId) {
 
 
 /*更新中・・・*/
-function toggleDots() {
-    const dotsElement = document.getElementById("dots");
-    dotsElement.style.visibility = dotsElement.style.visibility === "hidden" ? "visible" : "hidden";
-}
-
-function startBlinking() {
-    const intervalId = setInterval(toggleDots, 500); // 点滅間隔（ミリ秒）
-    setTimeout(() => clearInterval(intervalId), 5000); // 5秒後に点滅停止
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    startBlinking();
-});
-
-/*日付*/
-function updateDate() {
-    const currentDateElement = document.getElementById("currentDate");
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const formattedDate = `${year}.${month}.${day}`;
-    currentDateElement.textContent = "更新：" + formattedDate;
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    updateDate();
-    setInterval(updateDate, 1000); // 1秒ごとに日付を更新
-});
-
-//
 const dotsElement = document.getElementById('dots');
 const dots = ['.', '..', '...'];
 
@@ -62,3 +31,20 @@ function animateDots() {
 }
 
 animateDots();
+
+
+/*日付*/
+function updateDate() {
+    const currentDateElement = document.getElementById("currentDate");
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const formattedDate = `${year}.${month}.${day}`;
+    currentDateElement.textContent = "更新：" + formattedDate;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDate();
+    setInterval(updateDate, 1000); // 1秒ごとに日付を更新
+});
