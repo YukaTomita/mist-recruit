7ee042dde5924423ad415722f1b03f3bc2c60dc4
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-26 16:00:06 +0900

    syuusei

--- ranking-test.php
+++ ranking-test.php
@@ -2,6 +2,110 @@
 <html>
 <head>
     <title>投票ページ</title>
+    <style>
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .item {
+            width: 45%;
+            margin: 5px;
+            padding: 10px;
+            border: 1px solid #ccc;
+        }
+        .bar {
+            background-color: lightblue;
+            height: 20px;
+        }
+        .img-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .img-container img {
+            max-width: 100px;
+            margin-top: 5px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    // データベースへの接続情報
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "selection";
+
+    // データベースへの接続
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // 接続エラーの確認
+    if ($conn->connect_error) {
+        die("接続エラー: " . $conn->connect_error);
+    }
+
+    // 投票フォームが送信された場合
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["items"])) {
+        $selectedItems = $_POST["items"];
+
+        foreach ($selectedItems as $itemId) {
+            // 選択された項目の投票数を1つ増やす
+            $sql = "UPDATE items SET votes = votes + 1 WHERE id = " . (int)$itemId;
+            $conn->query($sql);
+        }
+    }
+
+    // 項目のリストと順位情報を取得
+    $sql = "SELECT id, name, votes FROM items ORDER BY votes DESC";
+    $result = $conn->query($sql);
+
+    $items = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $items[] = $row;
+        }
+    }
+
+    // データベース接続を閉じる
+    $conn->close();
+    ?>
+
+    <h1>項目ランキング</h1>
+    <div class="container">
+        <?php foreach ($items as $item): ?>
+            <div class="item">
+                <div class="bar" style="width: <?php echo $item['votes'] * 10; ?>px;"></div>
+                <div class="img-container">
+                    <?php if ($item['votes'] > 0 && $item['votes'] <= 3): ?>
+                        <img src="rank_<?php echo $item['votes']; ?>.png" alt="Rank <?php echo $item['votes']; ?>">
+                    <?php endif; ?>
+                </div>
+                <?php echo $item['name']; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <h2>投票する項目</h2>
+    <form method="post" action="">
+        <div class="container">
+            <?php foreach ($items as $item): ?>
+                <label>
+                    <input type="checkbox" name="items[]" value="<?php echo $item['id']; ?>">
+                    <?php echo $item['name']; ?>
+                </label>
+            <?php endforeach; ?>
+        </div>
+        <br>
+        <input type="submit" value="投票">
+    </form>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>投票ページ</title>
 </head>
 <body>
     <?php
