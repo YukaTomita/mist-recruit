1f7e108cf0e7139a8ce6eb282bd9d6b6bf247d36
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-28 10:06:36 +0900

    ranking

--- ranking-test.php
+++ ranking-test.php
@@ -1,65 +1,198 @@
+<?php
+// データベース接続情報
+$host = 'localhost';
+$db = 'sport';
+$user = 'root';
+$password = 'root';
+
+// データベースに接続
+$conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+
+// タイムゾーンを設定
+date_default_timezone_set('Asia/Tokyo');
+
+// 投票結果のリセット処理、1分間ボタンが押せなくなる、指定した時間に開いていないとリセットされない
+if (date('H:i') === '03:00') {
+    // 投票数をゼロにリセットするクエリを実行
+    $resetQuery = "TRUNCATE TABLE votes";
+    $resetStmt = $conn->prepare($resetQuery);
+    $resetStmt->execute();
+
+    // 投票履歴を削除するクエリを実行
+    $deleteQuery = "TRUNCATE TABLE votes_history";
+    $deleteStmt = $conn->prepare($deleteQuery);
+    $deleteStmt->execute();
+}
+
+// スポーツの一覧を取得
+$query = "SELECT * FROM sports";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$sports = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// フォームが送信された場合の処理
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $selectedSport = $_POST['sport'];
+
+    // 投票履歴をチェック
+    $userIp = $_SERVER['REMOTE_ADDR'];
+    $query = "SELECT * FROM votes_history WHERE user_ip = :user_ip";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':user_ip', $userIp);
+    $stmt->execute();
+    $voteHistory = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$voteHistory) {
+        // 投票結果をデータベースに保存
+        $query = "INSERT INTO votes (sport_id) VALUES (:sport_id)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':sport_id', $selectedSport);
+        $stmt->execute();
+
+        // 投票履歴を保存
+        $query = "INSERT INTO votes_history (user_ip, sport_id) VALUES (:user_ip, :sport_id)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':user_ip', $userIp);
+        $stmt->bindParam(':sport_id', $selectedSport);
+        $stmt->execute();
+    }
+
+    // ページをリロードして再投稿を防止するためのリダイレクト
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+// 投票結果の取得とランキングの作成
+$query = "SELECT sport_id, COUNT(*) AS count FROM votes GROUP BY sport_id ORDER BY count DESC";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$voteResults = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// ランキングデータの生成
+$ranking = [];
+$rank = 1;
+$prevCount = null;
+foreach ($voteResults as $result) {
+    $sportId = $result['sport_id'];
+    $count = $result['count'];
+
+    $sportName = "";
+    foreach ($sports as $sport) {
+        if ($sport['id'] == $sportId) {
+            $sportName = $sport['name'];
+            break;
+        }
+    }
+
+    // 同率順位の場合、前の順位と投票数を比較して順位を設定
+    if ($prevCount !== null && $prevCount !== $count) {
+        $rank++;
+    }
+
+    $ranking[] = [
+        'rank' => $rank,
+        'sportName' => $sportName,
+        'count' => $count
+    ];
+
+    $prevCount = $count;
+}
+
+// 投票済みかどうかをチェック
+$userIp = $_SERVER['REMOTE_ADDR'];
+$query = "SELECT * FROM votes_history WHERE user_ip = :user_ip";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(':user_ip', $userIp);
+$stmt->execute();
+$voteHistory = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// データベース接続のクローズ
+$conn = null;
+?>
+
+
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-    <title>ランキング縦棒グラフ</title>
-    <style>
-        .graph-container {
-            display: flex;
-            flex-direction: column-reverse;
-            width: 150px;
-            height: 300px;
-            border: 1px solid #000;
-        }
-
-        .graph-bar {
-            flex: 1;
-            background-color: blue;
-            margin: 1px;
-        }
-
-        .item-name {
-            writing-mode: vertical-rl;
-            text-orientation: upright;
-            text-align: center;
-            margin: 1px;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <title>test</title>
 </head>
 <body>
-    <h1>ランキング縦棒グラフ</h1>
-    <div class="graph-container">
-        <?php
-    // データベースへの接続情報
-        $servername = "localhost";
-        $username = "root";
-        $password = "root";
-        $dbname = "ranking";
+    <p class="font-style-words text-center">現在のランキング</p>
+    <!-- エンジニアが選ぶ企業のポイント　ランキング -->
+    <div class="wrapper">
+        <canvas id="barChart"></canvas>
 
-        // データベースに接続
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        <script>
+            // データの準備
+            const labels = [];
+            const data = [];
 
-        // 接続エラーの確認
-        if ($conn->connect_error) {
-            die("接続に失敗しました: " . $conn->connect_error);
-        }
+            <?php foreach ($results as $result) : ?>
+                labels.push('<?php echo $result['name']; ?>');
+                data.push(<?php echo $result['count']; ?>);
+            <?php endforeach; ?>
 
-        // ランキングデータの取得
-        $sql = "SELECT item_name FROM ranking ORDER BY rank ASC";
-        $result = $conn->query($sql);
+            // チャートの描画
+            const ctx = document.getElementById('barChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: '投票数',
+                        data: data,
+                        backgroundColor: 'transparent', // 棒グラフの背景色
+                        borderColor: '#FF2D2D', // 棒グラフの枠線の色
+                        borderWidth: 2 // 棒グラフの枠線の太さ
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            max: Math.max(...data) + 2, // 最大値 + 2 を設定
+                            title: {
+                                display: false,
+                                text: '投票数'
+                            }
+                        }
+                    }
+                }
+            });
+        </script>
 
-        // ランキングデータを縦棒グラフとして表示
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '<div class="graph-bar" style="height: ' . ($row["rank"] * 30) . 'px;"></div>';
-                echo '<div class="item-name">' . $row["item_name"] . '</div>';
-            }
-        } else {
-            echo "データがありません。";
-        }
+        <!-- 隙間 -->
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
 
-        // データベース接続を閉じる
-        $conn->close();
-        ?>
+        <div class="cercle">ランキングに参加する</div>
+        <div class="Arrow-Bottom"></div>
+        <div class="Arrow-Bottom"></div>
+
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+
+        <p class="font-style-comments2 txt line-height">キャリアアップで転職される際に、重要視されるポイントを下記よりお選びください。<br>※複数選択可能</p>
+        <?php if (!$voteHistory) : ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <?php foreach ($options as $option) : ?>
+                    <div class="option">
+                        <input type="checkbox" id="option<?php echo $option['id']; ?>" name="vote[]" value="<?php echo $option['id']; ?>">
+                        <label for="option<?php echo $option['id']; ?>"><?php echo $option['name']; ?></label>
+                    </div>
+                <?php endforeach; ?>
+                <button class="post-btn" type="submit">投票する</button>
+            </form>
+        <?php else : ?>
+            <p class="vote-message asterisk">※すでに投票済みです。</p>
+        <?php endif; ?>
     </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<script src="js\header.js"></script>
+<script src="js/upperclassman.js"></script>
+
 </body>
 </html>
