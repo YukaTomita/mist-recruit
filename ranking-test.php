865f0b7efbfbb40bc278829fba9fd44d94938d6f
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-28 11:44:17 +0900

    rank

--- ranking-test.php
+++ ranking-test.php
@@ -1,3 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>好きなスポーツランキング</title>
+    <style>
+        .ranking {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        .ranking img {
+            width: 30px;
+            height: 30px;
+            margin-right: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>好きなスポーツランキング</h1>
+    <form method="post">
+        <label for="sport1">サッカー</label>
+        <input type="radio" name="sport" value="サッカー" id="sport1"><br>
+
+        <!-- 他の投票項目も同様に10個繰り返す -->
+        <label for="sport2">野球</label>
+        <input type="radio" name="sport" value="野球" id="sport2"><br>
+
+        <label for="sport3">バスケットボール</label>
+        <input type="radio" name="sport" value="バスケットボール" id="sport3"><br>
+
+        <!-- 他の投票項目も同様に10個繰り返す -->
+
+        <input type="submit" value="投票">
+    </form>
+
+    <h2>投票結果</h2>
+    <?php
+    // データベース接続情報
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "rank";
+    
+    // データベースからトップ3のランキングを取得
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("接続エラー: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT sport_name, votes FROM sports_ranking ORDER BY votes DESC LIMIT 3";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $rank = 1;
+        while ($row = $result->fetch_assoc()) {
+            echo "<div class='ranking'>";
+            echo "<img src='" . $rank . "th_place_icon.png' alt='" . $rank . "位'>";
+            echo "<strong>" . $rank . "位</strong> " . $row["sport_name"] . " - " . $row["votes"] . "票";
+            echo "</div>";
+            $rank++;
+        }
+    } else {
+        echo "<p>データがありません。</p>";
+    }
+
+    $conn->close();
+    ?>
+</body>
+</html>
+
 <?php
 // データベース接続情報
 $servername = "localhost";
@@ -69,5 +140,57 @@ $conn->close();
         <input type="submit" value="投票">
     </form>
 
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>好きなスポーツランキング</title>
+    <style>
+        .ranking {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        .ranking img {
+            width: 30px;
+            height: 30px;
+            margin-right: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>好きなスポーツランキング</h1>
+    <form method="post">
+        <label for="sport1">サッカー</label>
+        <input type="radio" name="sport" value="サッカー" id="sport1"><br>
+
+        <!-- 他の投票項目も同様に10個繰り返す -->
+        <label for="sport2">野球</label>
+        <input type="radio" name="sport" value="野球" id="sport2"><br>
+
+        <label for="sport3">バスケットボール</label>
+        <input type="radio" name="sport" value="バスケットボール" id="sport3"><br>
+
+        <!-- 他の投票項目も同様に10個繰り返す -->
+
+        <input type="submit" value="投票">
+    </form>
+
+    <h2>投票結果</h2>
+    <div class='ranking'>
+        <img src='1st_place_icon.png' alt='1位'>
+        <strong>1位</strong> <!-- 1位のスポーツ名はPHPで表示する -->
+    </div>
+    <div class='ranking'>
+        <img src='2nd_place_icon.png' alt='2位'>
+        <strong>2位</strong> <!-- 2位のスポーツ名はPHPで表示する -->
+    </div>
+    <div class='ranking'>
+        <img src='3rd_place_icon.png' alt='3位'>
+        <strong>3位</strong> <!-- 3位のスポーツ名はPHPで表示する -->
+    </div>
 </body>
 </html>
