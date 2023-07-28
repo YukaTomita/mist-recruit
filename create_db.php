e4964a4b372a59784b0bb4249ed28b1963036028
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-28 12:21:20 +0900

    test

--- create_db.php
+++ create_db.php
@@ -1,55 +1,258 @@
 <?php
 // データベース接続情報
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "rank";
+$host = 'localhost';
+$db = 'sport';
+$user = 'root';
+$password = 'root';
 
 // データベースに接続
-$conn = new mysqli($servername, $username, $password);
+$conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
 
-// 接続エラーの確認
-if ($conn->connect_error) {
-    die("接続エラー: " . $conn->connect_error);
+// タイムゾーンを設定
+date_default_timezone_set('Asia/Tokyo');
+
+// 投票結果のリセット処理、1分間ボタンが押せなくなる、指定した時間に開いていないとリセットされない
+if (date('H:i') === '03:00') {
+    // 投票数をゼロにリセットするクエリを実行
+    $resetQuery = "TRUNCATE TABLE votes";
+    $resetStmt = $conn->prepare($resetQuery);
+    $resetStmt->execute();
+
+    // 投票履歴を削除するクエリを実行
+    $deleteQuery = "TRUNCATE TABLE votes_history";
+    $deleteStmt = $conn->prepare($deleteQuery);
+    $deleteStmt->execute();
 }
 
-// データベースが存在しない場合は作成
-$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
-$conn->query($sql);
+// スポーツの一覧を取得
+$query = "SELECT * FROM sports";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$sports = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$conn->close();
+// フォームが送信された場合の処理
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $selectedSport = $_POST['sport'];
 
-// データベースに接続
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // 投票履歴をチェック
+    $userIp = $_SERVER['REMOTE_ADDR'];
+    $query = "SELECT * FROM votes_history WHERE user_ip = :user_ip";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':user_ip', $userIp);
+    $stmt->execute();
+    $voteHistory = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// 接続エラーの確認
-if ($conn->connect_error) {
-    die("接続エラー: " . $conn->connect_error);
+    if (!$voteHistory) {
+        // 投票結果をデータベースに保存
+        $query = "INSERT INTO votes (sport_id) VALUES (:sport_id)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':sport_id', $selectedSport);
+        $stmt->execute();
+
+        // 投票履歴を保存
+        $query = "INSERT INTO votes_history (user_ip, sport_id) VALUES (:user_ip, :sport_id)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':user_ip', $userIp);
+        $stmt->bindParam(':sport_id', $selectedSport);
+        $stmt->execute();
+    }
+
+    // ページをリロードして再投稿を防止するためのリダイレクト
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
 }
 
-// テーブルが存在しない場合は作成
-$sql = "CREATE TABLE IF NOT EXISTS sports_ranking (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sport_name VARCHAR(50) NOT NULL,
-    votes INT DEFAULT 0
-)";
+// 投票結果の取得とランキングの作成
+$query = "SELECT sport_id, COUNT(*) AS count FROM votes GROUP BY sport_id ORDER BY count DESC";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$voteResults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$conn->query($sql);
+// ランキングデータの生成
+$ranking = [];
+$rank = 1;
+$prevCount = null;
+foreach ($voteResults as $result) {
+    $sportId = $result['sport_id'];
+    $count = $result['count'];
 
-// 初期データの挿入
-$sql = "INSERT INTO sports_ranking (sport_name, votes) VALUES ('サッカー', 0), ('野球', 0), ('バスケットボール', 0)";
-$conn->query($sql);
+    $sportName = "";
+    foreach ($sports as $sport) {
+        if ($sport['id'] == $sportId) {
+            $sportName = $sport['name'];
+            break;
+        }
+    }
 
-$conn->close();
+    // 同率順位の場合、前の順位と投票数を比較して順位を設定
+    if ($prevCount !== null && $prevCount !== $count) {
+        $rank++;
+    }
+
+    $ranking[] = [
+        'rank' => $rank,
+        'sportName' => $sportName,
+        'count' => $count
+    ];
+
+    $prevCount = $count;
+}
+
+// 投票済みかどうかをチェック
+$userIp = $_SERVER['REMOTE_ADDR'];
+$query = "SELECT * FROM votes_history WHERE user_ip = :user_ip";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(':user_ip', $userIp);
+$stmt->execute();
+$voteHistory = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// データベース接続のクローズ
+$conn = null;
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="ja">
+
 <head>
-    <title>データベース初期設定とデータ挿入</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <meta name="description" content="株式会社MIST solution - トップページ 株式会社ミストソリューションは、異なった業界との接点を持つことで化学反応を起こし、
+    幅広いニーズにより的確にお応えできる、常に進化しているIT企業です。">
+    <meta name="keywords" content="株式会社ミストソリューション,ミストソリューション,MISTsolution,ミスト" />
+    <meta name="copyright" content="© 1997, 2023 mistsolution. All Rights Reserved.">
+    <meta name="format-detection" content="telephone=no">
+    <!-- OGP -->
+    <meta property="og:url" content="https://www.mistnet.co.jp">
+    <meta property="og:title" content="株式会社MIST solution | WEBサイト" />
+    <meta property="og:site_name" content="株式会社MIST solution | WEBサイト">
+    <meta name="og:description" content="株式会社MIST solution - トップページ 株式会社ミストソリューションは、異なった業界との接点を持つことで化学反応を起こし、
+    幅広いニーズにより的確にお応えできる、常に進化しているIT企業です。">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="ja-JP">
+    <meta property="og:image" content="assets/img/mist-ogp.jpg">
+    <meta name="twitter:card" content="summary" />
+    <!-- favicon -->
+    <link rel="icon" href="img/favicon.ico">
+    <title>testtest</title>
+    <link rel="stylesheet" href="CSS/reset.css">
+    <link rel="stylesheet" href="CSS/common.css">
+    <link rel="stylesheet" href="CSS/newcomer.css">
+    <link rel="stylesheet" href="CSS/expert.css">
 </head>
+
 <body>
-    <h1>データベース初期設定とデータ挿入</h1>
-    <p>データベースの初期設定とデータの挿入が完了しました。</p>
-</body>
-</html>
+    <!-- header -->
+    <header class="header flex-box">
+        <h1 class="site-title">
+            <a href="#!">
+                <img src="img/グループ 1315.png" alt="ロゴ">
+            </a>
+        </h1>
+        <a href="#!"><img class="header-twitter" src="img/white background.png" alt="Twitter"></a>
+        <a href="#!" class="header-entry">エントリー</a>
+        <nav class="header-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="#!">Service</a></li>
+                <li class="nav-item"><a href="#!">About</a></li>
+                <li class="nav-item"><a href="#!">News</a></li>
+                <li class="nav-item"><a href="#!">Conetact</a></li>
+                <li class="nav-item"><a href="#!">Recruit</a></li>
+            </ul>
+        </nav>
+        <button class="burger-btn">
+            <span class="bars">
+                <span class="bar bar_top"></span>
+                <span class="bar bar_mid"></span>
+                <span class="bar bar_bottom"></span>
+            </span>
+        </button>
+        <span class="burger-musk"></span>
+    </header>
+
+    <!-- トップ画像と中央配置 -->
+    <img class="top-img" src="img\newcomer.jpg" alt="画像">
+    <div class="wrapper">
+
+        <!-- 隙間 -->
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+
+
+        <!-- 投票機能 -->
+        <p class="font-style-title">質　問</p>
+        <hr class="border-line">
+        <p class="font-style-words2">「あなたの好きなスポーツは何ですか？」</p>
+            <div class="load">
+                <div id="loadingText">上位5つを更新中<span class="dots">...</span></div>
+            </div>
+
+            <div class="gap-control-probram"></div>
+            <div class="gap-control-probram"></div>
+
+            <div class="load">
+                <p id="currentDate"></p>
+            </div>
+
+            <div class="gap-control-probram"></div>
+            <div class="gap-control-probram"></div>
+
+            <p class="font-style-words text-center">現在のランキング</p>
+
+        <!-- 隙間 -->
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+
+        <?php if (!empty($ranking) && $voteHistory) : ?>
+            <div class="ranking">
+                <?php $count = 0; ?>
+                <?php foreach ($ranking as $rankData) : ?>
+                    <?php if ($count >= 5) break; ?> <!-- 5つ以上の要素は表示しない -->
+                    <div class="bar-graph text-align">
+                        <p class="rank"><span><?php echo $rankData['rank']; ?></span>位</p>
+                        <p class="sportName"><?php echo $rankData['sportName']; ?></p>
+                    </div>
+                    <?php $count++; ?>
+                <?php endforeach; ?>
+            </div>
+        <?php else : ?>
+            <p class="ranking asterisk">※投票するとランキングが表示されます。</p>
+        <?php endif; ?>
+
+        <!-- 隙間 -->
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+
+        <div class="cercle">ランキングに参加する</div>
+        <div class="Arrow-Bottom"></div>
+        <div class="Arrow-Bottom"></div>
+
+        <div class="gap-control-probram"></div>
+        <div class="gap-control-probram"></div>
+
+        <!-- 投票欄 -->
+        <div class="font-style-comments2 line-height">
+            <p>「学生時代していた。」もしくは、「個人でしていた。」など、該当するスポーツを下記からお選びください。（※複数されていた方は、一番長く在籍していたスポーツをお選びください。）
+            <div class="vote">
+                <?php if ($voteHistory) : ?>
+                    <p class="asterisk">※既に投票済みです。</p>
+                <?php else : ?>
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <?php foreach ($sports as $sport) : ?>
+                            <button class="sport-button" type="submit" name="sport" value="<?php echo $sport['id']; ?>">
+                                <?php echo $sport['name']; ?>
+                            </button>
+                        <?php endforeach; ?>
+                    </form>
+                <?php endif; ?>
+            </div>
+            <P class="font-style-comments2">
+                エンジニアに何故スポーツ？と思う方もいるかもしれませんが、エンジニアはスポーツで培った個々のポジションの役割、チームワークなど、今回社員になったSESのルーキーたちは、
+                皆スポーツをしていて、現在の業務や仕事に取り組む際の姿勢のベースになっています。エンジニアの現場経験がなかったり、経験が短期だったとしても、実際の現場では人間力も
+                強い武器になってきます。
+            </p>
+        </div>
+
