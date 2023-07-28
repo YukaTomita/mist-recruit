3c07b6d64ed63ce0da845b30d9a79cbb7d44e343
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-28 10:36:51 +0900

    rank

--- update_votes.php
+++ update_votes.php
@@ -1,43 +1,60 @@
 <?php
-    // データベースへの接続情報
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "ranking";
+// データベース接続情報
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "rank";
 
-// データベースに接続
-$conn = new mysqli($servername, $username, $password, $dbname);
+// フォームからの投票データを取得
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["sport"])) {
+        $selected_sport = $_POST["sport"];
 
-// 接続エラーの確認
-if ($conn->connect_error) {
-    die("接続に失敗しました: " . $conn->connect_error);
-}
+        // データベースに接続
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // フォームから送信されたデータを取得
-    if (isset($_POST["items"]) && is_array($_POST["items"])) {
-        $items = $_POST["items"];
-        $itemIds = implode(",", $items);
+        // 接続エラーの確認
+        if ($conn->connect_error) {
+            die("接続エラー: " . $conn->connect_error);
+        }
 
-        // 投票数を更新
-        $sql = "UPDATE ranking SET votes = votes + 1 WHERE id IN ($itemIds)";
+        // 選択されたスポーツの投票数を更新
+        $sql = "UPDATE sports_ranking SET votes = votes + 1 WHERE sport_name = '$selected_sport'";
         $conn->query($sql);
+
+        $conn->close();
     }
 }
 
-// 更新された投票結果を取得
-$sql = "SELECT item_name, votes FROM ranking ORDER BY votes DESC";
+// データベースからトップ3のランキングを取得
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("接続エラー: " . $conn->connect_error);
+}
+
+$sql = "SELECT sport_name, votes FROM sports_ranking ORDER BY votes DESC LIMIT 3";
 $result = $conn->query($sql);
-
-// 投票結果をHTMLとして返す
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo '<p>' . $row["item_name"] . ': ' . $row["votes"] . '票</p>';
-    }
-} else {
-    echo "データがありません。";
-}
-
-// データベース接続を閉じる
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>好きなスポーツランキング結果</title>
+</head>
+<body>
+    <h1>好きなスポーツランキング結果</h1>
+    <ol>
+        <?php
+        // ランキング結果の表示
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>" . $row["sport_name"] . " - " . $row["votes"] . "票</li>";
+            }
+        } else {
+            echo "<li>データがありません。</li>";
+        }
+        ?>
+    </ol>
+</body>
+</html>
