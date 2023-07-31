7b80e9d4811a9eb0c15cd24ee96ff6cf6c6db3db
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-31 13:44:07 +0900

    ランキング

--- create_db.php
+++ create_db.php
@@ -108,8 +108,13 @@ $stmt->bindParam(':user_ip', $userIp);
 $stmt->execute();
 $voteHistory = $stmt->fetch(PDO::FETCH_ASSOC);
 
-//last voting date
-
+// 投票済みかどうかをチェック
+$userIp = $_SERVER['REMOTE_ADDR'];
+$query = "SELECT created_at FROM votes_history WHERE user_ip = :user_ip ORDER BY created_at DESC LIMIT 1";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(':user_ip', $userIp);
+$stmt->execute();
+$lastVotingDate = $stmt->fetchColumn();
 
 // データベース接続のクローズ
 $conn = null;
@@ -167,6 +172,16 @@ $conn = null;
             <div class="gap-control-probram"></div>
 
             <p>更新: <?php echo $last_voting_date; ?></p>
+
+            <?php
+            if ($lastVotingDate) {
+                // $lastVotingDateを利用して必要な処理を行う
+                echo "最終投票日時： " . $lastVotingDate;
+            } else {
+                // 投票履歴がない場合の処理
+                echo "まだ投票がありません。";
+            }
+            ?>
 
             <div class="gap-control-probram"></div>
             <div class="gap-control-probram"></div>
