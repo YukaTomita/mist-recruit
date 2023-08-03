b38b9dd61f1ef7a710ba6f5331d65c8ebf9f475d
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-08-03 13:44:29 +0900

    n-popup

--- newcomer.php
+++ newcomer.php
@@ -302,7 +302,9 @@ $conn = null;
                                 <p class="pop-text">ラグビーでのチームワークが、プロジェクト進行のほうれんそうの習慣など、
                                     業務に役立っていると思います。またラグビーは個々のポジションが明確で、
                                     今の仕事でも自分が何をしなければならないかなど責任をはたすところで活きてます。</p>
-                        </div>
+                            </div>
+
+                        <img class="open-img" src="img/グループ 1822.png" alt="サンプル画像">
                     </div>
                     <p class="newcomer-text">僕はラグビー<br>をしてました。</p>
                     <button class="newcomer-btn1">もっと見る</button>
